3da71890296a9159aae28eda996fd84a88c1afb0
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-25 11:55:44 +0330

    cleaning the code and put codes on components

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
-import { ImSearch } from "react-icons/im";
-import { FaListUl } from "react-icons/fa";
-
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
@@ -16,6 +13,8 @@ import {
 } from "../helpers/helper";
 
 import styles from "./styles/ProductsPage.module.css";
+import SearchBox from "../components/SearchBox";
+import SideBar from "../components/SideBar";
 
 function ProductsPage() {
   const [search, setSearch] = useState("");
@@ -38,31 +37,9 @@ function ProductsPage() {
     setDisplayed(finalProducts);
   }, [query]);
 
-  const searchHandler = () => {
-    setQuery((query) => createQueryObject(query, { search }));
-  };
-
-  const categoryHandler = (event) => {
-    const { tagName } = event.target;
-    const category = event.target.innerText.toLowerCase();
-
-    if (tagName !== "LI") return;
-    setQuery((query) => createQueryObject(query, { category }));
-  };
-
   return (
     <>
-      <div>
-        <input
-          type="text"
-          placeholder="search..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
-        />
-        <button onClick={searchHandler}>
-          <ImSearch />
-        </button>
-      </div>
+      <SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
       <div className={styles.container}>
         <div className={styles.products}>
           {!displayed.length && <Loader />}
@@ -70,19 +47,7 @@ function ProductsPage() {
             <Card key={product.id} data={product} />
           ))}
         </div>
-        <div>
-          <div>
-            <FaListUl />
-            <p>Categories</p>
-            <ul onClick={categoryHandler}>
-              <li>All</li>
-              <li>Electronics</li>
-              <li>Jewelery</li>
-              <li>Men's Clothing</li>
-              <li>Women's Clothing</li>
-            </ul>
-          </div>
-        </div>
+        <SideBar setQuery={setQuery} query={query} />
       </div>
     </>
   );
