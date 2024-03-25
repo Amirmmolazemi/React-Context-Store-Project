15fea427799e7cf95002d9bba02aae7c71e29d5a
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-25 21:21:03 +0330

    add context and layout to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import DetailsPage from "./Pages/DetailsPage";
 import CheckoutPage from "./Pages/CheckoutPage";
 import NotFoundPage from "./Pages/NotFoundPage";
 import ProductContext from "./context/ProductContext";
+import CartProvider from "./context/CartContext";
+import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <>
-      <ProductContext>
-        <Routes>
-          <Route path="/" element={<Navigate to="/products" replace />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/:id" element={<DetailsPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/*" element={<NotFoundPage />} />
-        </Routes>
-      </ProductContext>
+      <CartProvider>
+        <ProductContext>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Navigate to="/products" replace />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/products/:id" element={<DetailsPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/*" element={<NotFoundPage />} />
+            </Routes>
+          </Layout>
+        </ProductContext>
+      </CartProvider>
     </>
   );
 }
