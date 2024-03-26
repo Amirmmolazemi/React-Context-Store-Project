60d775d8735a6a6a3d9bc08a505808ecf948e5a3
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-26 11:43:10 +0330

    shows the product detail when you click on detail button

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -1,5 +1,40 @@
+import { Link, useParams } from "react-router-dom";
+import { SiOpenproject } from "react-icons/si";
+import { IoMdPricetag } from "react-icons/io";
+import { FaArrowLeft } from "react-icons/fa";
+
+import { useProductsDetails } from "../context/ProductContext";
+import Loader from "../components/Loader.jsx";
+import styles from "./styles/DetailsPage.module.css";
+
 function DetailsPage() {
-  return <>details</>;
+  const { id } = useParams();
+  const productDetails = useProductsDetails(+id);
+  if (!productDetails) return <Loader />;
+
+  return (
+    <div className={styles.container}>
+      <img src={productDetails.image} alt={productDetails.title} />
+      <div className={styles.information}>
+        <h3 className={styles.title}>{productDetails.title}</h3>
+        <p className={styles.description}>{productDetails.description}</p>
+        <p className={styles.category}>
+          <SiOpenproject />
+          {productDetails.category}
+        </p>
+        <div>
+          <span className={styles.price}>
+            <IoMdPricetag />
+            {productDetails.price} $
+          </span>
+          <Link to="/products">
+            <FaArrowLeft />
+            <span>Back To Shop</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DetailsPage;
