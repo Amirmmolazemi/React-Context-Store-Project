48fdfd5660f10ae546be03de206006d39aa0db55
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-26 11:43:45 +0330

    checkout page is a component that shows the items and also the status of your cart

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -1,7 +1,36 @@
+import BasketCard from "../components/BasketCard";
+import BasketSidebar from "../components/BasketSidebar";
+import { useCart } from "../context/CartContext";
+
+import styles from "./styles/CheckoutPage.module.css";
+
 function CheckoutPage() {
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => dispatch({ type, payload });
+
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.container}>
+        <h3>Your Basket is Emptey</h3>
+      </div>
+    );
+  }
+
   return (
-    <div>CheckoutPage</div>
-  )
+    <div className={styles.container}>
+      <BasketSidebar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
+        {state.selectedItems.map((product) => (
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default CheckoutPage
+export default CheckoutPage;
