de849aba37292861344d5e44fd3283ba456f9c6b
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-26 11:45:55 +0330

    add a custom hook to give us the details of the product by giving it the ID

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -30,5 +30,11 @@ const useProducts = () => {
   return products;
 };
 
+const useProductsDetails = (id) => {
+  const products = useContext(productContext);
+  const result = products.find((product) => product.id === id);
+  return result;
+};
+
 export default ProductContext;
-export { useProducts };
+export { useProducts, useProductsDetails };
